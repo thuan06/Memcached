a93a78d2151ab34da0766d949ea88c6b4c029dab
thuan06 <thuan0610@gmail.com>
2016-05-17 10:02:54 +0700

    Update memcached_import.py

--- memcached_import.py
+++ memcached_import.py
@@ -1,1 +1,19 @@
+import memcache
+
+host = "43.239.220.132"
+port = 11211
+path = "/root/test/out.txt"
+
+lines = [line.rstrip('\n') for line in open('%s'%path)]
+n=len(lines)
+d=int(n/2)
+
+mc = memcache.Client(['%s:%d' %(host,port)],debug=0)
+if n>0:
+        for i in range(d):
+                mc.set("%s" %(lines[i]), "%s" %(lines[i+d]))
+        print "Key imported"
+else:
+        print "No new key imported"
+
 

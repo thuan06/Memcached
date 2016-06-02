ed1916baeee072395ee7db14957389f811700203
thuan06 <thuan0610@gmail.com>
2016-06-02 12:00:54 +0700

    Update memcached_export.py

--- memcached_export.py
+++ memcached_export.py
@@ -1,13 +1,27 @@
+#!/usr/bin/env python
 import subprocess
+import memcache
 
-host = "43.239.220.132"
-port = 11211
+host1 = "43.239.220.132"
+port1 = 11211
 path = "/tmp/out.txt"
 
-#---------------Save data-------------
-key = subprocess.call("memdump --servers=%s" %host,stdout=open("%s"%path, 'w'),shell=True)
+host2 = "43.239.220.133"
+port2 = 11211
+
+#--------------- Key export to file -------------
+key = subprocess.call("memdump --servers=%s" %host1,stdout=open("%s"%path, 'w'),shell=True)
 print "Kex Exported"
-fo = open("%s" %path,"a")
-value = subprocess.call("memcat --servers=%s `memdump --servers=%s`" %(host,host),stdout=open("%s"%path, 'a'), shell=True)
-print "Value Exported"
-fo.close()
+#--------------Save key:value to a dic ------------
+m1 = memcache.Client(['%s:%d' %(host1,port1)],debug=0)
+
+lines = [line.rstrip('\n') for line in open('%s'%path)]
+print lines
+mem={}
+for i in lines:
+        value=m1.get(i)
+        mem[i]=value
+#-----------Export dict to new server --------------
+m2 = memcache.Client(['%s:%d' %(host2,port2)],debug=0)
+for i in mem.keys():
+        m2.set(i, mem.get(i))
